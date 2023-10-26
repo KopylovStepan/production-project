694241409c64993f388f6e5344308ab0005c5d23
Kopylov Stepan <kopylov-s.a@mail.ru>
2023-10-26 21:25:18 +0700

    fix some problems

--- src/pages/ManPage/ui/MainPage.async.tsx
+++
@@ -1,4 +0,0 @@
-import {lazy} from 'react'
-
-export const MainPageAsync = lazy(() => {
-    return import("./MainPage")})
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,28 +1,28 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './Button.module.scss'
-import {ButtonHTMLAttributes, FC} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
+import cls from './Button.module.scss';
 
-export enum ThemeButton{
-    CLEAR = 'clear'
+export enum ThemeButton {
+    CLEAR = 'clear',
 }
-
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    className?:string;
-    theme?: ThemeButton
+    className?: string;
+    theme?: ThemeButton;
 }
 
-export const Button:FC<ButtonProps> = (props) => {
+export const Button: FC<ButtonProps> = (props) => {
     const {
         className,
         children,
         theme,
         ...otherProps
-    } = props
+    } = props;
 
     return (
         <button
-            className={classNames(cls.Button,{}, [className, cls[theme]])}
+            type="button"
+            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
             {...otherProps}
         >
             {children}
